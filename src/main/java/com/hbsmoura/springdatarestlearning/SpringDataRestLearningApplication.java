b9977fa8136d3cabbf2300fa84c4346986e0b07d
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-06-07 19:58:36 -0300

    Initial commit

---
+++ src/main/java/com/hbsmoura/springdatarestlearning/SpringDataRestLearningApplication.java
@@ -0,0 +1,13 @@
+package com.hbsmoura.springdatarestlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbsmoura/springdatarestlearning/SpringDataRestLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbsmoura.springdatarestlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
