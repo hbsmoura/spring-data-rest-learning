c31c00571a7fdfb617333094ce428e301ffb8d76
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-06-07 19:59:48 -0300

    Easy implementation

---
+++ src/main/java/com/hbsmoura/springdatarestlearning/model/Person.java
@@ -0,0 +1,47 @@
+package com.hbsmoura.springdatarestlearning.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO PERSON VALUES (1, 'Zé', 'da Manga')
+INSERT INTO PERSON VALUES (2, 'Flavinho', 'do Pneu')
+INSERT INTO PERSON VALUES (3, 'Nego', 'Dilson')
+INSERT INTO PERSON VALUES (4, 'Pregnant', 'of Taubateh')
