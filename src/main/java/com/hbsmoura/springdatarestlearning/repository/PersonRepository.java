290090eec769c40b84edf93c43c9532fe451da91
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-06-08 19:24:50 -0300

    Extra configurations

--- src/main/java/com/hbsmoura/springdatarestlearning/repository/PersonRepository.java
+++ src/main/java/com/hbsmoura/springdatarestlearning/repository/PersonRepository.java
@@ -1,9 +1,9 @@
-package com.hbsmoura.springdatarestlearning;
+package com.hbsmoura.springdatarestlearning.repository;
 
 import com.hbsmoura.springdatarestlearning.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource(path = "collaborators")
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
